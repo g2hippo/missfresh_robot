2cd6e44fbaec0b05da9fcdc20da1d2f1e6aef4d9
Lewis <g2hippo@qq.com>
2018-10-03 06:39:07 +0800

    add 1 sku of p-HBlbxg-2g

--- missfresh_robot.py
+++ missfresh_robot.py
@@ -10,7 +10,8 @@ sku_list = ['p-hbqxspdt-4g', 'p-tjspt-2g', 'p-hbspdt-2g',
             "p-hbcts-2g", "p-tjyncts-2g",
             "p-hbjfpt-1h", "p-tjjfpt-1h",
             "p-hbxjkelxl-5g",
-            "p-hbdldz1j-1h-new", "p-tjdldz1j-2h"
+            "p-hbdldz1j-1h-new", "p-tjdldz1j-2h",
+            "p-HBlbxg-2g"
             ]
 address_code = '110106'
 station_code = 'MRYX|mryx_jgmpx'
